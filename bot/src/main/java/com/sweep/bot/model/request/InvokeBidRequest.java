0252876e5c3137d511d410b0e93f35aa3f2469b8
pradumnkr98 <pradumnkr98@gmail.com>
2021-12-12 14:08:19 +0530

    Invoke bid request and response body implemented

--- bot/src/main/java/com/sweep/bot/model/request/InvokeBidRequest.java
+++ bot/src/main/java/com/sweep/bot/model/request/InvokeBidRequest.java
@@ -1,5 +1,28 @@
 package com.sweep.bot.model.request;
 
+import com.sweep.bot.model.constants.PlayerState;
+import com.sweep.bot.model.entity.BoardStatus;
+import com.sweep.bot.model.entity.PlayerInfo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvokeBidRequest {
 
+    private String id;
+
+    private PlayerState state;
+
+    private List<PlayerInfo> players;
+
+    private BoardStatus board;
+
+    private Integer turn;
 }
--- bot/src/main/java/com/sweep/bot/model/response/InvokeBidResponse.java
+++ bot/src/main/java/com/sweep/bot/model/response/InvokeBidResponse.java
@@ -1,5 +1,15 @@
 package com.sweep.bot.model.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvokeBidResponse {
 
+    private String bid;
 }
